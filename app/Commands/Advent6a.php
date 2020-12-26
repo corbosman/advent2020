e809934c91988ed71935bc1145d6c8965dbc3133
Cor Bosman <cor@xs4all.net>
2020-12-26 12:54:08 +0100

    wip

--- app/Commands/Advent6a.php
+++ app/Commands/Advent6a.php
@@ -14,7 +14,6 @@ class Advent6a extends Command
     {
         $sum = collect(explode("\n\n", file_get_contents(storage_path('6.txt'))))
             ->map(fn($line) => str_replace(["\n", " "], ["", ""], $line))
-            ->dump()
             ->map(fn($line) => collect(str_split($line))->unique()->count())
             ->sum();
     }
